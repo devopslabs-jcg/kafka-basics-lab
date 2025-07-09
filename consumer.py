51b86e11443b671062327915e4f05ac444a62a36
jangk34 <jangk34@naver.com>
2025-07-10 02:11:58 +0900

    토픽을 구독하여 새로운 메시지 수신 및 출력

---
+++ consumer.py
@@ -0,0 +1,19 @@
+from kafka import KafkaConsumer
+
+# Kafka Consumer 설정 (Docker 외부에서 접속)
+consumer = KafkaConsumer(
+    'hello-kafka',
+    bootstrap_servers=['localhost:29092'],
+    auto_offset_reset='earliest', # 처음부터 모든 메시지를 읽음
+    group_id='my-consumer-group',
+    value_deserializer=lambda m: m.decode('utf-8')
+)
+
+print("✅ Start consuming messages...")
+try:
+    for message in consumer:
+        print(f"Received: '{message.value}' from Topic '{message.topic}'")
+except KeyboardInterrupt:
+    print("\n Stop consuming messages.")
+finally:
+    consumer.close()
